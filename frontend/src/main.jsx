600362338e38c6b20ddab612edab68fda6232fdd
moun <mounirfendou@gmail.com>
2026-06-16 05:39:34 +0200

    modif main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'      // Provider = enveloppe Redux pour toute l'app
-import { store } from './store/store'      // notre boîte de données // notre store Redux 
+import { store } from './store/store'      // le store Redux : notre boîte de données globale
 import App from './App'                     // composant racine qui contient toutes les routes
 import './assets/main.css'                 // CSS global pour toute l'app
 
 ReactDOM.createRoot(
   document.getElementById('root')          // pointe vers dans index.html
 ).render(
-  <React.StrictMode>              {  /*  Provider rend le store accessible dans TOUS les composants enfants*/}
-                                   {/*  Sans lui useSelector et useDispatch ne fonctionnent pas */}
-    <Provider store={store}>        {  /* indispensable */ }
+  <React.StrictMode>     {/* outil de détection d'erreurs en développement */}          
+    { /*  Provider rend le store accessible dans TOUS les composants enfants*/}
+    { /*  Sans lui useSelector et useDispatch ne fonctionnent pas */}
+    <Provider store={store}>    {  /* indispensable */ }
       <App />
     </Provider>
   </React.StrictMode>
